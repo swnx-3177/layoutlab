4d032d2db230b9a338876a41855c5c5dada3dade
swnx-3177 <73655917+swnx-3177@users.noreply.github.com>
2020-11-06 17:57:26 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Lay Out</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row row-height">
+            <div class="col-md-6 cell">
+                <div class="cell-body1">
+                    <h3>1.</h3>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row height-100">
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body2">
+                                    <h3>2.</h3>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body2">
+                                    <h3>3.</h3>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 33%;">
+                                <div class="cell-body2">
+                                    <h3>4.</h3>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="row height-100">
+                            <div class="col-12 cell" style="height: 66%;">
+                                <div class="cell-body3">
+                                    <h3>5.</h3>
+                                </div>
+                            </div>
+                            <div class="col-12 cell" style="height: 34%;">
+                                <div class="cell-body3">
+                                    <h3>6.</h3>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
